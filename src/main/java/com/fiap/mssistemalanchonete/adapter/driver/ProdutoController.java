09cea98e2847f77889ab09584076c3dede2b846d
Guimaj <deco-jorge@hotmail.com>
2024-05-05 19:46:18 -0300

    atualizando controllers produto e cliente

--- src/main/java/com/fiap/mssistemalanchonete/adapter/driver/ProdutoController.java
+++ src/main/java/com/fiap/mssistemalanchonete/adapter/driver/ProdutoController.java
@@ -2,30 +2,39 @@ package com.fiap.mssistemalanchonete.adapter.driver;
 
 import com.fiap.mssistemalanchonete.core.applications.userCase.IProdutoUserCase;
 import com.fiap.mssistemalanchonete.core.domain.produto.Produto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/products")
 public class ProdutoController {
 
     @Autowired
     private IProdutoUserCase service;
 
-    @PostMapping(value = "/produto", consumes = {"*"})
-    public ResponseEntity<Object> crateControllerValidation(
-            @RequestHeader(value = "Authorization", required = true) String token,
-            @RequestHeader(value = "Content-Type", required = true) String contentType,
+    @Operation(
+            description = "Cria novo produto",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Produto criado com sucesso!")
+            }
+    )
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> createProduct(
             @RequestBody final Produto produto) throws Exception {
-        return ResponseEntity.accepted().body(service.setProduto(produto));
+        return ResponseEntity.status(201).body(service.setProduto(produto));
     }
 
-    @GetMapping(value = "/produto/{categoria}", consumes = {"*"})
-    public ResponseEntity<Object> crateControllerValidation(
-            @RequestHeader(value = "Authorization", required = true) String token,
-            @RequestHeader(value = "Content-Type", required = true) String contentType,
-            @PathVariable final String categoria) throws Exception {
+    @Operation(
+            description = "Retorna produtos pela categoria",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Produtos retornados com sucesso!")
+            }
+    )
+    @GetMapping(value = "/{category}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> crateControllerValidation(@PathVariable final String categoria) throws Exception {
         return ResponseEntity.accepted().body(service.getProdutoPorCategoria(categoria));
     }
-
 }
